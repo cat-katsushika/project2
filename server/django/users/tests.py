a6695acc436e192a11fa1976f4ad61a2583a01ca
morita-y9 <xiongxisentian@gmail.com>
2024-03-02 00:32:11 +0900

    サインアップテスト

--- server/django/users/tests.py
+++ server/django/users/tests.py
@@ -1,25 +1,3 @@
-from rest_framework import status
-from rest_framework.test import APIClient
-
-from django.test import TestCase
-
-from .models import User
-
+# from django.test import TestCase
 
 # Create your tests here.
-class SignUpAPItest(TestCase):
-    def setUp(self):
-        self.signup_url = "http://localhost:8000/v1/users/create/"
-        self.client = APIClient()
-
-    def test_signup_success(self):
-        data = {"username": "testuser", "password": "testpassword"}
-        response = self.client.post(self.signup_url, data, format="json")
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-
-    def test_signup_with_existing_username(self):
-        User.objects.create_user(username="existinguser", password="existingpassword")
-        data = {"username": "existinguser", "password": "newpassword"}
-        response = self.client.post(self.signup_url, data, format="json")
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertIn("A user with that username already exists.", response.data["username"])

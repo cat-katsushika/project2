a78801a6706b6d4ec369ca6866958d19c2f08795
morita-y9 <xiongxisentian@gmail.com>
2024-03-02 00:21:22 +0900

    テスト+修正

--- server/django/users/serializers.py
+++ server/django/users/serializers.py
@@ -7,19 +7,13 @@ User = get_user_model()
 
 
 class UserSerializer(serializers.ModelSerializer):
-    id = serializers.IntegerField(read_only=True)
-    password_confirm = serializers.CharField(write_only=True, required=True, style={"input_type": "password"})
+    id = serializers.UUIDField(read_only=True)
     password = serializers.CharField(write_only=True, style={"input_type": "password"})
 
     class Meta:
         model = User
-        fields = ["id", "username", "password", "password_confirm"]
+        fields = ["id", "username", "password"]
         extra_kwargs = {"password": {"write_only": True}}
-
-    def validate(self, data):
-        if data["password"] != data["password_confirm"]:
-            raise serializers.ValidationError("確認用パスワードと一致しません．")
-        return data
 
     def create(self, validated_data):
         user = User.objects.create_user(

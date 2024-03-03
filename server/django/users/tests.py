81f6fa22b2fa33442b6cd86dd6183417e5e2b0fa
morita-y9 <xiongxisentian@gmail.com>
2024-03-03 17:07:53 +0900

    ログインテスト

--- server/django/users/tests.py
+++ server/django/users/tests.py
@@ -7,7 +7,7 @@ from django.urls import reverse
 from .models import User
 
 
-class SignUpAPItest(TestCase):
+class SignUpAPITest(TestCase):
     def setUp(self):
         self.signup_url = reverse("users:signup")
         self.client = APIClient()
@@ -23,3 +23,21 @@ class SignUpAPItest(TestCase):
         response = self.client.post(self.signup_url, data, format="json")
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertIn("A user with that username already exists.", response.data["username"])
+
+
+class LoginAPITest(TestCase):
+    def setUp(self):
+        self.login_url = reverse("users:login")
+        self.client = APIClient()
+        User.objects.create_user(username="testuser", password="testpassword")
+
+    def test_login_success(self):
+        data = {"username": "testuser", "password": "testpassword"}
+        response = self.client.post(self.login_url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_login_failure(self):
+        data = {"username": "notexistinguser", "password": "notexistingtestpassword"}
+        response = self.client.post(self.login_url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertIn("No active account found with the given credentials", str(response.data))
--- server/django/users/urls.py
+++ server/django/users/urls.py
@@ -7,6 +7,6 @@ from .views import SignUpAPIView
 app_name = "users"
 urlpatterns = [
     path("v1/users/create/", SignUpAPIView.as_view(), name="signup"),
-    path("v1/users/login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("v1/users/login/", TokenObtainPairView.as_view(), name="login"),
     path("v1/users/token/refresh", TokenRefreshView.as_view(), name="token_refresh"),
 ]

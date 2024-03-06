6d74229ea9ede999780a1e6117e9421547f4dcb4
Hanaka Iwasaki <h.iwasaki.22.9.631@4nonome.com>
2024-03-06 20:47:19 +0900

    2024.03.06 修正

--- server/django/teams/tests.py
+++ server/django/teams/tests.py
@@ -1,6 +1,7 @@
 from rest_framework import status
 from rest_framework.test import APITestCase
-from rest_framework_simplejwt.serializers import TokenObtainSerializer
+
+# from rest_framework_simplejwt.serializers import TokenObtainSerializer
 
 from django.urls import reverse
 from teams.models import Team

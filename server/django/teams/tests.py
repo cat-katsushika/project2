5a918a5628fa1d4912fe09cd7b4de92451d458ee
Hanaka Iwasaki <h.iwasaki.22.9.631@4nonome.com>
2024-05-18 22:32:38 +0900

    2024.05.18 テストの追加

--- server/django/teams/tests.py
+++ server/django/teams/tests.py
@@ -28,6 +28,8 @@ class TeamCreateAPITest(APITestCase):
         response = self.client.post(self.teamcreate_url, data, format="json")
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data, data)
+        team = Team.objects.get(name="testteam")
+        self.assertEqual(team.users.get(username="testuser"), self.user)
 
     def test_teamcreate_with_empty_name(self):
         data = {

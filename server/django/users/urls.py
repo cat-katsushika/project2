c03e4dc8589fb92e97d3994ecfaec00ae06c9bf2
pg-endokenta <k.endo@4nonome.com>
2024-05-31 21:40:39 +0900

    add slash

--- server/django/users/urls.py
+++ server/django/users/urls.py
@@ -8,6 +8,6 @@ app_name = "users"
 urlpatterns = [
     path("v1/users/create/", SignUpAPIView.as_view(), name="signup"),
     path("v1/users/login/", TokenObtainPairView.as_view(), name="login"),
-    path("v1/users/token/refresh", TokenRefreshView.as_view(), name="token_refresh"),
+    path("v1/users/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("v1/users/change_name/", ChangeUsernameAPIView.as_view(), name="change_username"),
 ]

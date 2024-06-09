7f18481201db440d1054e2171919001864555bf0
morita-y9 <xiongxisentian@gmail.com>
2024-06-09 21:31:42 +0900

    EOF

--- server/django/users/views.py
+++ server/django/users/views.py
@@ -61,6 +61,8 @@ class DeleteAccountsAPIView(APIView):
                 Task.objects.create(user=next_user, team=team, status=Task.Status.IN_PROGRESS)
         user.delete()
         return Response(status=status.HTTP_200_OK)
+    
+
         
         
         
